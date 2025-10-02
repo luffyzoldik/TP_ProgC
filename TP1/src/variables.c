576b8e1eea21269ae0ea76ff122d1e3ddd9cc4af
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 15:04:30 +0200

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,25 @@
+
+
+
+#include <stdio.h>
+
+int main(void) {
+    int n = 5;     
+    int i, j;
+
+    for (i = 1; i <= n; i++) {
+        for (j = 0; j < n - i; j++) {
+            putchar(' ');
+        }
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        putchar('\n');
+    }
+
+    printf("Voici ta pyramide de hauter %d.\n", n);
+    return 0;
+}
