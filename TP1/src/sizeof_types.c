f60c4d1e649e10bd742bfdade8382d51adbcbd90
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 10:53:49 +0200

    Update sizeof_types.c

---
+++ TP1/src/sizeof_types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Tailles des types de donnees en octets\n");
+
+    printf("char : %d octets\n", sizeof(char));
+    printf("signed char : %d octets\n", sizeof(signed char));
+    printf("unsigned char : %d octets\n\n", sizeof(unsigned char));
+
+    printf("short : %d octets\n", sizeof(short));
+    printf("signed short : %d octets\n", sizeof(signed short));
+    printf("unsigned short : %d octets\n", sizeof(unsigned short));
+
+    
