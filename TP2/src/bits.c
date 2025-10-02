bf68fed0647e1e85355dc312339fdb19af7ba4a5
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 16:07:45 +0200

    Update bits.c

---
+++ TP2/src/bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    unsigned int d = 0xF0008000; 
+    int nb_bits = sizeof(d) * 8;  
+    int pos1 = 4;
+    int pos2 = 20;
+
+  
+    int bit_index1 = nb_bits - pos1;
+    int bit_index2 = nb_bits - pos2;
+
+    int bit1 = (d >> bit_index1) & 1;
+    int bit2 = (d >> bit_index2) & 1;
+
+    if (bit1 == 1 && bit2 == 1) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    return 0;
+}
