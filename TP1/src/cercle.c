ad99852d633d3a5745e947e99d15d1582a61f108
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 10:33:29 +0200

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
-double pi = 3.1416;
-double r = 6;
-    printf("l'air du cercle = %d ", pirr);
-    printf("Le perimetre du cercle = %d", pi*(r+r));
+int main(void){
+    double pi = 3.1416;
+    double r = 6;
+    printf("l'air du cercle = %.4f\n", pi * r * r);
+    printf("Le perimetre du cercle = %.4f\n", pi * (r + r));
+    return 0;
 }
-
