80d22c043aac2aa931742ffbfb247479fa822696
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 11:02:00 +0200

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -10,5 +10,18 @@ int main() {
     printf("short : %d octets\n", sizeof(short));
     printf("signed short : %d octets\n", sizeof(signed short));
     printf("unsigned short : %d octets\n", sizeof(unsigned short));
+    
+    printf("int : %d octets\n", sizeof(int));
+    printf("signed int : %d octets\n", sizeof(signed int));
+    printf("unsigned int : %d octets\n\n", sizeof(unsigned int));
+
+    printf("long int : %d octets\n", sizeof(long int));
+    printf("signed long int : %d octets\n", sizeof(signed long int));
+    printf("unsigned long int : %d octets\n\n", sizeof(unsigned long int));
+
+    printf("long long int : %d octets\n", sizeof(long long int));
+    printf("signed long long int : %d octets\n", sizeof(signed long long int));
+    printf("unsigned long long int : %d octets\n\n", sizeof(unsigned long long int));
     return 0;
 } 
+
