23bd31e5a178390afa878d61e53ae8e0e415c55e
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 11:32:52 +0200

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16;
+    int b = 3;
+    
+    printf("a = %d\n", a);
+    printf("b = %d\n\n", b);
+    
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n\n", a % b);
+    printf("a == b : %d\n", a == b);
+    printf("a > b : %d\n", a > b);
+    
+    return 0;
+}
