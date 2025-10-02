89578a1acf53dee5f0e76e5e84c3ea403f1aaaf6
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 12:04:29 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int compteur = 5;
+    int i, j;
+    
+
+    for (i = 1; i <= compteur; i++) {
+        for (j = 1; j <= i; j++) {
+            if (j == 1 || j == i || i == compteur) {
+                printf("* ");
+            } else {
+                printf("# ");
+            }
+        }
+        printf("\n");
+    }
+}
