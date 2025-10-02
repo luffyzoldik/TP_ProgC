31abdc95edeca5e9205067daf8fa979830e66c24
Luffy_Zoldik <94780148+luffyzoldik@users.noreply.github.com>
2025-10-02 12:18:45 +0200

    Update boucles.c

---
+++ TP1/src/boucles.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int somme = 0;
+    
+    for (int i = 1; i <= 1000; i++) {
+        
+        if (i % 11 == 0) {
+            continue;
+        }
+        
+        if (i % 5 == 0 || i % 7 == 0) {
+            somme = somme + i;
+        }
+        
+        if (somme > 5000) {
+            printf("La somme a depasse 5000 !\n");
+            break;
+        }
+    }
+    
+    printf("Somme finale : %d\n", somme);
+    
+    return 0;
+}
